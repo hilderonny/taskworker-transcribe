43cd2f342b6139430b9c1f72d7b5984b64810663
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2024-09-07 21:08:27 +0200

    Added first try. DOes not work with Python 3.12

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # taskworker-transcribe
-Worker for taskbridge which can handle "translate" tasks
+
+Worker for taskbridge which can handle tasks of type `transcribe`.
+
+## Task format
+
+```json
+{
+    ...
+    "type" : "transcribe",
+    "worker" : "ROG",
+    "file" : "123456789",
+    ...
+    "result" : {
+        "language" : "en",
+        "texts" : [
+            "Line 1",
+            "",
+            "Line 3"
+        ],
+        "device" : "cuda",
+        "duration" : 12,
+        "repository" : "https://github.com/hilderonny/taskworker-transcribe",
+        "version" : "1.0.0",
+        "library" : "fasterwhisper-0.8.15",
+        "model" : "large-v2"
+    }
+}
+```
+
+The `type` must be `transcribe`.
+
+`worker` contains the unique name of the worker which processed the task.
+
+The worker expects a `file` property defining the filename which contains the audio to transcribe.
+
+When the worker finishes the task, it sends back a `result` property. This property is an object. It contains an array `texts` with the transcribed audio broken into chunks. The result also contains the detected `language`'s 2 digit ISO code.
+
+## Installation
+
+Currently this worker only works with Nvidia GPUs (no CPU).
+First install Python 3.12. The run the following commands in the folder of the downloaded repository.
+
+```
+python -m venv python-venv
+python-venv/Scripts/activate
+pip install faster-whisper==0.9.0
+pip install --force-reinstall ctranslate2==3.24.0
+```
+
+
+cudnn_ops_infer64_8.dll
+cudnn_cnn_infer64_8.dll
+zlibwapi.dll
+cublasLt64_11.dll
+cublas64_11.dll
+
+https://developer.nvidia.com/rdp/cudnn-archive#a-collapse811-111
+
+https://developer.nvidia.com/cudnn-downloads
+
+Download and install the CUDA Toolkit from https://developer.nvidia.com/cuda-downloads.
+
+Next you need to copy `cudnn_ops_infer64_8.dll`, `cudnn_cnn_infer64_8.dll`, `cublas64_11.dll`, `cublasLt64_11.dll` and `zlibwapi.dll` (Can be found in subdirectory `dll`) into the directory `python-venv/Lib/site-packages/ctranslate2`.
+
+## Running
+
+Running the program the first time, ai models with about 4 GB (depending on the used model) get downloaded automatically.
+
+```sh
+python transcribe.py --taskbridgeurl http://192.168.178.39:42000/ --worker ROG --model large-v2
+```
+
+## Literature
+
+1. https://github.com/SYSTRAN/faster-whisper
+
---
+++ transcribe.py
@@ -0,0 +1,112 @@
+from importlib.metadata import version
+import time
+import json
+import requests
+import datetime
+import argparse
+import os
+
+REPOSITORY = "https://github.com/hilderonny/taskworker-transcribe"
+VERSION = "1.2.0"
+LIBRARY = "faster-whisper-" + version("faster-whisper")
+DEVICE = "cuda"
+APIVERSION = "v2"
+LOCAL_MODEL_PATH = "./models/faster-whisper"
+LOCAL_FILE_PATH = "./temp"
+
+print(f'Transcriber Version {VERSION}')
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--taskbridgeurl', type=str, action='store', required=True, help='Root URL of the API of the task bridge to use, e.g. https://taskbridge.ai/')
+parser.add_argument('--model', type=str, action='store', help='Whisper model size to use. Can be "tiny", "base", "small", "medium", "large-v2" or "large-v3".')
+parser.add_argument('--version', '-v', action='version', version=VERSION)
+parser.add_argument('--worker', type=str, action='store', required=True, help='Unique name of this worker')
+args = parser.parse_args()
+
+WORKER = args.worker
+print(f'Worker name: {WORKER}')
+TASKBRIDGEURL = args.taskbridgeurl
+if not TASKBRIDGEURL.endswith("/"):
+    TASKBRIDGEURL = f"{TASKBRIDGEURL}/"
+APIURL = f"{TASKBRIDGEURL}api/{APIVERSION}/"
+print(f'Using API URL {APIURL}')
+
+MODEL = args.model
+print(f'Using whisper model {MODEL}')
+
+# Prepare temp path
+os.makedirs(LOCAL_FILE_PATH, exist_ok=True)
+
+# Load AI
+from faster_whisper import WhisperModel    
+compute_type = 'float16' # if USEGPU else 'int8'
+#device = 'cuda' # if USEGPU else 'cpu'
+whisper_model = WhisperModel( model_size_or_path = MODEL, device = DEVICE, local_files_only = False, compute_type = compute_type, download_root = LOCAL_MODEL_PATH )
+
+def process_file(file_path):
+    start_time = datetime.datetime.now()
+    result = {}
+    try:
+        print("Processing file " + file_path)
+        print("Transcribing")
+        transcribe_segments_generator, transcribe_info = whisper_model.transcribe(file_path, task = "transcribe")
+        transcribe_segments = list(map(lambda segment: { "start": segment.start, "end": segment.end, "text": segment.text }, transcribe_segments_generator))
+        original_language = transcribe_info.language
+        result["language"] = original_language
+        result["texts"] = transcribe_segments
+    except Exception as ex:
+        print(ex)
+    end_time = datetime.datetime.now()
+    result["duration"] = (end_time - start_time).total_seconds()
+    return result
+
+def check_and_process():
+    start_time = datetime.datetime.now()
+    take_request = {}
+    take_request["type"] = "transcribe"
+    take_request["worker"] = WORKER
+    response = requests.post(f"{APIURL}tasks/take/", json=take_request)
+    print(response)
+    if response.status_code != 200:
+        return False
+    task = response.json()
+    taskid = task["id"]
+    print(json.dumps(task, indent=2))
+
+    file_response = requests.get(f"{APIURL}tasks/file/{taskid}")
+    print(file_response)
+    local_file_path = os.path.join(LOCAL_FILE_PATH, taskid)
+    with open(local_file_path, 'wb') as file:
+        file.write(file_response.content)
+
+    result_to_report = {}
+    result_to_report["result"] = {}
+    result_to_report["result"]["texts"] = process_file(local_file_path)
+
+    result_to_report["result"]["language"] = None
+    end_time = datetime.datetime.now()
+    result_to_report["result"]["device"] = DEVICE
+    result_to_report["result"]["duration"] = (end_time - start_time).total_seconds()
+    result_to_report["result"]["repository"] = REPOSITORY
+    result_to_report["result"]["version"] = VERSION
+    result_to_report["result"]["library"] = LIBRARY
+    result_to_report["result"]["model"] = MODEL
+    print(json.dumps(result_to_report, indent=2))
+    print("Reporting result")
+    requests.post(f"{APIURL}tasks/complete/{taskid}/", json=result_to_report)
+    print("Done")
+    return True
+
+try:
+    print('Ready and waiting for action')
+    while True:
+        task_was_processed = False
+        try:
+            task_was_processed = check_and_process()
+        except Exception as ex:
+            print(ex)
+        if task_was_processed == False:
+            time.sleep(3)
+except Exception:
+    pass
